ca9242c5c098ca5d829f16b0da363056b9e32116
Anastasia Drozd <anastasia.drozd.2411@gmail.com>
2022-01-10 21:40:58 +0200

    implemented better km miles converter

--- basics/t2.py
+++ basics/t2.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 
-from lib import miles_to_km
+from lib import km_to_miles, miles_to_km
 
 def main():
-    print ('введите значение в милях')
-    miles = input()
-    miles = int(miles)
-    km = miles_to_km(miles)
+    print('введите значение в милях (10miles) или километрах (10km)')
+    value = input()
+    if value.find("km") != -1:
+        pos = value.find("km")
+        km = value[:pos] 
+        km = int(km)
+        miles = km_to_miles(km)
+        print("значение в километрах",km,"равно", miles,"миль")
 
-    message = str(miles) + " миль равно " + str(km) + " километров "
-    print(message) 
-
-    print (f"{miles} миль равно {km} километров")
+    if value.find("miles") != -1:
+        pos = value.find("miles")
+        miles = value[:pos]
+        miles = int(miles)
+        km = miles_to_km(miles)
+        print("значение в милях", miles,"равно",km,"километров")
+    
 
 main()
 
